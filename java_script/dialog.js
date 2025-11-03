98579f3d626620091a974ba69e1328fc9e7fc226
Rohric <emilmarsal@icloud.com>
2025-11-03 16:15:22 +0100

    fix dialog button

--- java_script/dialog.js
+++ java_script/dialog.js
@@ -3,6 +3,12 @@
   const openButton = document.querySelector('[data-dialog-open="imprintDialog"]');
   const closeButton = dialog?.querySelector('[data-dialog-close]');
 
+  openButton?.addEventListener('keydown', (event) => {
+    if (event.code === 'Space' || event.key === ' ' || event.key === 'Spacebar') {
+      event.preventDefault();
+    }
+  });
+
   openButton?.addEventListener('click', () => dialog?.showModal());
   closeButton?.addEventListener('click', () => dialog?.close());
   dialog?.addEventListener('cancel', (event) => {
